7cc2b5c5c9a22dbb36ea1077b746fe9da54b1dad
Volodymyr Hohunskyi <vgogunsky@griddynamics.com>
2024-02-16 11:09:46 +0200

    Initial Commit

---
+++ GithubClient/GithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientApp.swift
+//  GithubClient
+//
+//  Created by vgogunsky on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
